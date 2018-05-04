97d370451ffeaba89c3bc1e05576855039500516
Moemeister <00062716@uca.edu.sv>
2018-05-03 23:46:58 -0600

    orden ascendente de los nombres de los contactos

--- app/src/main/java/com/moesystems/contacts_parcial/MainActivity.java
+++ app/src/main/java/com/moesystems/contacts_parcial/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void addContacts() {
         try {
-            Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
             while (phones.moveToNext()) {
                 String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                 String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
